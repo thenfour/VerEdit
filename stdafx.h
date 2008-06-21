1cd4593f94a70961d60448c67c85035947d164bf
carl <carl@72871cd9-16e1-0310-933f-800000000000>
2008-06-21 23:38:48 +0000

    renaming libcc headers - to lowercase - with .hpp extension
    git-svn-id: https://subversion.assembla.com/svn/caste/personal/carl/VerEdit@664 72871cd9-16e1-0310-933f-800000000000

--- stdafx.h
+++ stdafx.h
@@ -12,10 +12,10 @@
 // TODO: reference additional headers your program requires here
 #import "libid:D63E0CE2-A0A2-11D0-9C02-00C04FC99C8E"// MSXML
 
-#include "..\libcc\libcc\StringUtil.h"
-#include "..\libcc\libcc\Result.h"
-#include "..\libcc\libcc\Blob.h"
-#include "..\libcc\libcc\Winapi.h"
+#include "..\libcc\libcc\StringUtil.hpp"
+#include "..\libcc\libcc\Result.hpp"
+#include "..\libcc\libcc\Blob.hpp"
+#include "..\libcc\libcc\Winapi.hpp"
 #include <iostream>
 #include <string>
 #include <list>
